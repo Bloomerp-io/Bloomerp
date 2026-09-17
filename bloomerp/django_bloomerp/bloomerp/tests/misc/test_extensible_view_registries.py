313c959f482dbb4bc5a72d259cb3e4a215080437
David Bloomer <bloomer.david@outlook.com>
2026-09-17 11:42:06 +0200

    Refactor import paths for DataviewTypeDefinition and update request parameters in DataViewTileRenderer

--- bloomerp/django_bloomerp/bloomerp/tests/misc/test_extensible_view_registries.py
+++ bloomerp/django_bloomerp/bloomerp/tests/misc/test_extensible_view_registries.py
@@ -1,6 +1,6 @@
 from django.test import RequestFactory, SimpleTestCase
 
-from bloomerp.dataviews.base import DataviewTypeDefinition
+from bloomerp.dataviews.definition import DataviewTypeDefinition
 from bloomerp.dataviews.registry import DATAVIEW_REGISTRY
 from bloomerp.dataviews.table.config import TableDataView
 from bloomerp.dataviews.table.renderer import TableDataviewRenderer
